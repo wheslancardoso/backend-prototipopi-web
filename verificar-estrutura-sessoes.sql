29303e9cc60e9451486a00c1472ad9cf86235bfd
wheslancardoso <wheslancardoso1@gmail.com>
2025-06-22 22:45:06 -0300

    chore: limpar arquivos temporários e de teste do diretório raiz
    - Remover 25 arquivos temporários (.md, .sh, .sql)
    - Manter apenas documentação essencial (README, MIGRACAO_WEB_TEATRO, DICAS_APRENDIZADO_WEB)
    - Manter apenas script principal do banco (database_moderno.sql)
    - Remover scripts de teste, correções SQL temporárias e guias de refatoração
    - Projeto agora com estrutura mais limpa e organizada

--- verificar-estrutura-sessoes.sql
+++
@@ -1,9 +0,0 @@
--- Verificar estrutura da tabela sessoes
-DESCRIBE sessoes;
-
--- Verificar dados atuais
-SELECT 'DADOS ATUAIS:' as info;
-SELECT id, nome, tipo_sessao, data_sessao, horario, ativo, evento_id FROM sessoes;
-
--- Verificar constraints
-SHOW CREATE TABLE sessoes; 
